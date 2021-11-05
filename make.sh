bf47f7b432fb62ea579f252a3048b68beadc0580
Tobias Kuhn <kuhntobias@gmail.com>
2021-11-05 14:16:09 +0100

    Update files

--- make.sh
+++ make.sh
@@ -12,7 +12,8 @@ cat np-graph.head.dot > np-graph.dot
 cat np-graph.csv \
   | sed '1d' \
   | sed -r 's/","(.*)","/" \1 "/g' \
-  | sed 's/""//g' \
+  | sed 's/""/"/g' \
+  | sed 's/ "$//g' \
   >> np-graph.dot
 
 cat np-graph.tail.dot >> np-graph.dot
